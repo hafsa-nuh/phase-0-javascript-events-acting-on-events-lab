f34e7c0f271ce16e3729d048f798e949461530dc
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-06 19:37:51 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+// let moveDodgerLeft = document.getElementById('dodger')
+// moveDodgerLeft.style.left
+// document.addEventListener('keydown', function(e) {
+//     if (e.which === 37) {
+//       var leftNumbers = dodger.style.left.replace('px', '')
+//       var left = parseInt(leftNumbers, 10)
+  
+//       dodger.style.left = `${left - 1}px`
+//     }
+// })
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
